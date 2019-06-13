f164a2f1eef84427cd070824f1f8a115aa6e7655
Dennis <verissimodennis@gmail.com>
2019-06-13 01:20:11 -0300

    classe retangulo - executando exemplo

--- rectangle.dart
+++ rectangle.dart
@@ -1,23 +1,20 @@
-
-
 import 'dart:math';
 
-class Rectangle{
+class Rectangle {
   int width;
   int height;
   Point origin;
 
-
-  Rectangle({this.origin = const Point(0,0), this.width = 0, this.height = 0});
+  Rectangle({this.origin = const Point(0, 0), this.width = 0, this.height = 0});
 
   @override
-  String ToString () => 'Origin: (${origin.x}, ${origin.y}), width: $width, height: $height';
-
+  String toString() =>
+      'Origin: (${origin.x}, ${origin.y}), width: $width, height: $height';
 }
 
-main(){
+main() {
   print(Rectangle(origin: const Point(10, 20), width: 100, height: 200));
-  print(Rectangle(origin: const Point(10, 20)));
+  print(Rectangle(origin: const Point(10, 10)));
   print(Rectangle(width: 200));
   print(Rectangle());
 }
