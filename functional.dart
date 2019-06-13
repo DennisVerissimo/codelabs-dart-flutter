44fdde0f76a313812c48c9055c2cf6649530494f
Dennis <verissimodennis@gmail.com>
2019-06-13 02:10:24 -0300

    covertendo codigo imperativo para funcional

--- functional.dart
+++ functional.dart
@@ -2,7 +2,12 @@ String scream (int lenght) => "A${'a' * lenght}h!";
 
 main (){
   final values = [1, 2, 3, 5, 10, 50];
-  for (var lenght in values){
+  values.map(scream).forEach(print);
+  
+  
+  
+  
+  /*for (var lenght in values){
     print(scream(lenght));
-  }
+  }*/
 }
