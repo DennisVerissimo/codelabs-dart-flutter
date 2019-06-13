08ee1bba6446cd35e75031810cd450f220a10daf
Dennis <verissimodennis@gmail.com>
2019-06-13 02:15:26 -0300

    recursos iteraveis

--- functional.dart
+++ functional.dart
@@ -2,11 +2,12 @@ String scream (int lenght) => "A${'a' * lenght}h!";
 
 main (){
   final values = [1, 2, 3, 5, 10, 50];
-  values.map(scream).forEach(print);
+  
+  values.skip(1).take(3).map(scream).forEach(print);
   
   
-  
-  
+  /*values.map(scream).forEach(print); */
+   
   /*for (var lenght in values){
     print(scream(lenght));
   }*/
